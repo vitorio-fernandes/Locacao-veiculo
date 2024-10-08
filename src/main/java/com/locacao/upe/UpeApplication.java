ae3e4e7c5c2c22a5852e591be9287714c3465da8
vitorio-fernandes <vitoriofernandes593@gmail.com>
2024-10-08 11:54:02 -0300

    Estrutura de pastas

---
+++ src/main/java/com/locacao/upe/UpeApplication.java
@@ -0,0 +1,13 @@
+package com.locacao.upe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locacao/upe/UpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locacao.upe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
